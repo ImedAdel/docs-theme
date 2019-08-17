16f902dea3d9bf48b655f5762a40af008e49c22f
Imed Adel <imed.adel@sastec-group.com>
2019-08-17 16:52:18 +0100

    setup emotion

--- gatsby-theme-docs/gatsby-config.js
+++ gatsby-theme-docs/gatsby-config.js
@@ -24,5 +24,11 @@ module.exports = {
 				plugins: [],
 			},
 		},
+		{
+			resolve: `gatsby-plugin-emotion`,
+			options: {
+				// Accepts all options defined by `babel-plugin-emotion` plugin.
+			},
+		},
 	],
 }
