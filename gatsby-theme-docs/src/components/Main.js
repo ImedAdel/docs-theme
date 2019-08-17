319fb763730087aeb31bc9b650a8196082236921
Imed Adel <imed.adel@sastec-group.com>
2019-08-17 17:48:58 +0100

    some styling

--- gatsby-theme-docs/src/components/Main.js
+++ gatsby-theme-docs/src/components/Main.js
@@ -4,7 +4,6 @@ import { jsx } from '@emotion/core'
 const Main = ({ children }) => (
 	<div
 		css={{
-			padding: 16,
 			flexGrow: 99999,
 			flexBasis: 0,
 			minWidth: 320,
--- gatsby-theme-docs/src/components/Sidebar.js
+++ gatsby-theme-docs/src/components/Sidebar.js
@@ -1,16 +1,44 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
+import React from 'react'
+import { StaticQuery, Link } from 'gatsby'
 
-const Sidebar = ({ children }) => (
+const Sidebar = ({ data }) => (
 	<div
 		css={{
-			padding: 16,
+			padding: 0,
 			flexGrow: 1,
 			flexBasis: 256,
+			fontSize: `1.25vw`,
 		}}
 	>
-		{children}
+		<nav>
+			{data.allDirectory.nodes.map(({ relativePath }) => (
+				<Link
+					to={relativePath}
+					css={{
+						display: `block`,
+					}}
+				>
+					{relativePath}
+				</Link>
+			))}
+		</nav>
 	</div>
 )
 
-export default Sidebar
+const sidebarQuery = graphql`
+	{
+		allDirectory(sort: { fields: relativePath, order: ASC }) {
+			nodes {
+				relativePath
+			}
+		}
+	}
+`
+
+const StaticSidebar = () => (
+	<StaticQuery query={sidebarQuery} render={data => <Sidebar data={data} />} />
+)
+
+export default StaticSidebar
