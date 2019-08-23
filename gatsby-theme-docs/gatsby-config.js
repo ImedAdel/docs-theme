bb850b5dde56ff15b02bea041e71b7a23a7920cf
Imed Adel <imed.adel@sastec-group.com>
2019-08-23 14:05:57 +0100

    add gatsby-remark-copy-linked-files

--- gatsby-theme-docs/gatsby-config.js
+++ gatsby-theme-docs/gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
 				// GitHub Flavored Markdown mode (default: true)
 				gfm: true,
 				// Plugins configs
-				plugins: [],
+				plugins: [`gatsby-remark-copy-linked-files`],
 			},
 		},
 		{
